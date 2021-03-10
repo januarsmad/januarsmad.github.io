e23d6a2c89f5703f8b7e16b6b88cda725467365f
januarsmad <januar.sarmento@rebelworks.co>
2021-03-10 21:23:58 +0700

    update home

--- index.js
+++ index.js
@@ -3,30 +3,46 @@ var projectSection = $('.projects');
 var aboutSection = $('.about');
 var contactSection = $('.contact');
 
+var homeMenu = $('.home-menu')
 var projectsMenu = $('.projects-menu');
+var contactMenu = $('.contact-menu');
+var aboutMenu = $('.about-menu');
 
-$('.home').click(function() {
+homeMenu.click(function() {
   projectSection.removeClass('slide-left');
   aboutSection.removeClass('slide-right');
   contactSection.removeClass('slide-up');
+  contactMenu.removeClass('contact-menu-active');
+  aboutMenu.removeClass('about-menu-active');
+  projectsMenu.removeClass('projects-menu-active');
 })
 
-$('.projects-menu').click(function() {
+projectsMenu.click(function() {
   aboutSection.removeClass('slide-right');
   contactSection.removeClass('slide-up');
+  contactMenu.removeClass('contact-menu-active');
+  aboutMenu.removeClass('about-menu-active');
+
   projectSection.toggleClass('slide-left');
   projectsMenu.toggleClass('projects-menu-active')
 })
 
-$('.about-menu').click(function() {
+aboutMenu.click(function() {
   projectSection.removeClass('slide-left');
   contactSection.removeClass('slide-up');
+  projectsMenu.removeClass('projects-menu-active');
+  contactMenu.removeClass('contact-menu-active');
+
   aboutSection.toggleClass('slide-right');
+  aboutMenu.toggleClass('about-menu-active');
 })
 
-$('.contact-menu').click(function() {
+contactMenu.click(function() {
   projectSection.removeClass('slide-left');
   aboutSection.removeClass('slide-right');
   projectsMenu.removeClass('projects-menu-active');
+  aboutMenu.removeClass('about-menu-active');
+
+  contactMenu.toggleClass('contact-menu-active');
   contactSection.toggleClass('slide-up');
 })
